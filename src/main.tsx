05619a312b70750d9e5ef62497d0f86c4f46698c
Jordan-182 <jordan.p62@outlook.fr>
2025-04-10 18:05:31 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.tsx";
+import "./styles/index.css";
+
+// Pages import
+import { Article } from "./pages/Article.tsx";
+import { Home } from "./pages/Home.tsx";
+
+// Router creation
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/article",
+        element: <Article />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/Article.tsx
@@ -0,0 +1,7 @@
+export const Article = () => {
+  return (
+    <>
+      <h1>Article</h1>
+    </>
+  );
+};
